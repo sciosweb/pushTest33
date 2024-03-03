0f60a512716fce60a48657855d1476e563847169
gitTest <test333@test.com>
2024-03-03 17:12:29 +0000

    tet

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 # 두 수를 더하는 함수
 def add_numbers(a, b):
-    return a + b + c
+    return a + b
 
 # 사용자 입력 받기
 num1 = float(input("첫 번째 숫자를 입력하세요: "))
@@ -9,4 +9,5 @@ num2 = float(input("두 번째 숫자를 입력하세요: "))
 # 결과 출력
 result = add_numbers(num1, num2)
 print(f"{num1} + {num2} = {result}")
+
 
